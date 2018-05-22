5cc3f37b1bbd169a508d3748ababcecfb5e1e000
Jaicke <38104697+Jaicke@users.noreply.github.com>
2018-05-22 17:28:04 -0300

    Add files via upload

---
+++ ProjetoEstatistica v0.01/ProjetoEstatistica v0.01/Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace ProjetoEstatistica_v0._01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtN3 = new System.Windows.Forms.TextBox();
+            this.txtN4 = new System.Windows.Forms.TextBox();
+            this.txtN5 = new System.Windows.Forms.TextBox();
+            this.txtN6 = new System.Windows.Forms.TextBox();
+            this.txtN7 = new System.Windows.Forms.TextBox();
+            this.txtN8 = new System.Windows.Forms.TextBox();
+            this.txtN9 = new System.Windows.Forms.TextBox();
+            this.calcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtMediana = new System.Windows.Forms.TextBox();
+            this.txtModa = new System.Windows.Forms.TextBox();
+            this.txtDp = new System.Windows.Forms.TextBox();
+            this.txtSimetria = new System.Windows.Forms.TextBox();
+            this.txtPrimeiroQ = new System.Windows.Forms.TextBox();
+            this.txtSegundoQ = new System.Windows.Forms.TextBox();
+            this.txtTerceiroQ = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Probabilidade = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnLimpa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(29, 42);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 20);
+            this.txtN1.TabIndex = 0;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(144, 42);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 20);
+            this.txtN2.TabIndex = 1;
+            // 
+            // txtN3
+            // 
+            this.txtN3.Location = new System.Drawing.Point(260, 42);
+            this.txtN3.Name = "txtN3";
+            this.txtN3.Size = new System.Drawing.Size(100, 20);
+            this.txtN3.TabIndex = 2;
+            // 
+            // txtN4
+            // 
+            this.txtN4.Location = new System.Drawing.Point(29, 91);
+            this.txtN4.Name = "txtN4";
+            this.txtN4.Size = new System.Drawing.Size(100, 20);
+            this.txtN4.TabIndex = 3;
+            this.txtN4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txtN5
+            // 
+            this.txtN5.Location = new System.Drawing.Point(144, 91);
+            this.txtN5.Name = "txtN5";
+            this.txtN5.Size = new System.Drawing.Size(100, 20);
+            this.txtN5.TabIndex = 4;
+            // 
+            // txtN6
+            // 
+            this.txtN6.Location = new System.Drawing.Point(260, 91);
+            this.txtN6.Name = "txtN6";
+            this.txtN6.Size = new System.Drawing.Size(100, 20);
+            this.txtN6.TabIndex = 5;
+            // 
+            // txtN7
+            // 
+            this.txtN7.Location = new System.Drawing.Point(29, 136);
+            this.txtN7.Name = "txtN7";
+            this.txtN7.Size = new System.Drawing.Size(100, 20);
+            this.txtN7.TabIndex = 6;
+            // 
+            // txtN8
+            // 
+            this.txtN8.Location = new System.Drawing.Point(144, 136);
+            this.txtN8.Name = "txtN8";
+            this.txtN8.Size = new System.Drawing.Size(100, 20);
+            this.txtN8.TabIndex = 7;
+            this.txtN8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // txtN9
+            // 
+            this.txtN9.Location = new System.Drawing.Point(260, 136);
+            this.txtN9.Name = "txtN9";
+            this.txtN9.Size = new System.Drawing.Size(100, 20);
+            this.txtN9.TabIndex = 8;
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(121, 186);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(75, 23);
+            this.calcular.TabIndex = 9;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Média";
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Enabled = false;
+            this.txtMedia.Location = new System.Drawing.Point(86, 237);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(58, 20);
+            this.txtMedia.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Mediana";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Moda";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 340);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Desvio Padrão";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Simetria";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(222, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "3º quartil";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(222, 309);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "2º quartil";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(222, 273);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "1º quartil";
+            // 
+            // txtMediana
+            // 
+            this.txtMediana.Enabled = false;
+            this.txtMediana.Location = new System.Drawing.Point(86, 270);
+            this.txtMediana.Name = "txtMediana";
+            this.txtMediana.Size = new System.Drawing.Size(58, 20);
+            this.txtMediana.TabIndex = 19;
+            // 
+            // txtModa
+            // 
+            this.txtModa.Enabled = false;
+            this.txtModa.Location = new System.Drawing.Point(86, 306);
+            this.txtModa.Name = "txtModa";
+            this.txtModa.Size = new System.Drawing.Size(58, 20);
+            this.txtModa.TabIndex = 20;
+            // 
+            // txtDp
+            // 
+            this.txtDp.Enabled = false;
+            this.txtDp.Location = new System.Drawing.Point(86, 337);
+            this.txtDp.Name = "txtDp";
+            this.txtDp.Size = new System.Drawing.Size(58, 20);
+            this.txtDp.TabIndex = 21;
+            // 
+            // txtSimetria
+            // 
+            this.txtSimetria.Enabled = false;
+            this.txtSimetria.Location = new System.Drawing.Point(286, 233);
+            this.txtSimetria.Name = "txtSimetria";
+            this.txtSimetria.Size = new System.Drawing.Size(58, 20);
+            this.txtSimetria.TabIndex = 22;
+            // 
+            // txtPrimeiroQ
+            // 
+            this.txtPrimeiroQ.Enabled = false;
+            this.txtPrimeiroQ.Location = new System.Drawing.Point(286, 270);
+            this.txtPrimeiroQ.Name = "txtPrimeiroQ";
+            this.txtPrimeiroQ.Size = new System.Drawing.Size(58, 20);
+            this.txtPrimeiroQ.TabIndex = 23;
+            // 
+            // txtSegundoQ
+            // 
+            this.txtSegundoQ.Enabled = false;
+            this.txtSegundoQ.Location = new System.Drawing.Point(286, 306);
+            this.txtSegundoQ.Name = "txtSegundoQ";
+            this.txtSegundoQ.Size = new System.Drawing.Size(58, 20);
+            this.txtSegundoQ.TabIndex = 24;
+            // 
+            // txtTerceiroQ
+            // 
+            this.txtTerceiroQ.Enabled = false;
+            this.txtTerceiroQ.Location = new System.Drawing.Point(286, 337);
+            this.txtTerceiroQ.Name = "txtTerceiroQ";
+            this.txtTerceiroQ.Size = new System.Drawing.Size(58, 20);
+            this.txtTerceiroQ.TabIndex = 25;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 427);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Probabilidade
+            // 
+            this.Probabilidade.AutoSize = true;
+            this.Probabilidade.Location = new System.Drawing.Point(163, 410);
+            this.Probabilidade.Name = "Probabilidade";
+            this.Probabilidade.Size = new System.Drawing.Size(71, 13);
+            this.Probabilidade.TabIndex = 28;
+            this.Probabilidade.Text = "Probabilidade";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 426);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 20);
+            this.textBox2.TabIndex = 29;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(28, 410);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 13);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Digite um número";
+            // 
+            // btnLimpa
+            // 
+            this.btnLimpa.Location = new System.Drawing.Point(212, 186);
+            this.btnLimpa.Name = "btnLimpa";
+            this.btnLimpa.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpa.TabIndex = 31;
+            this.btnLimpa.Text = "Limpar";
+            this.btnLimpa.UseVisualStyleBackColor = true;
+            this.btnLimpa.Click += new System.EventHandler(this.BtnLimpa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 485);
+            this.Controls.Add(this.btnLimpa);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Probabilidade);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtTerceiroQ);
+            this.Controls.Add(this.txtSegundoQ);
+            this.Controls.Add(this.txtPrimeiroQ);
+            this.Controls.Add(this.txtSimetria);
+            this.Controls.Add(this.txtDp);
+            this.Controls.Add(this.txtModa);
+            this.Controls.Add(this.txtMediana);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.txtN9);
+            this.Controls.Add(this.txtN8);
+            this.Controls.Add(this.txtN7);
+            this.Controls.Add(this.txtN6);
+            this.Controls.Add(this.txtN5);
+            this.Controls.Add(this.txtN4);
+            this.Controls.Add(this.txtN3);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtN3;
+        private System.Windows.Forms.TextBox txtN4;
+        private System.Windows.Forms.TextBox txtN5;
+        private System.Windows.Forms.TextBox txtN6;
+        private System.Windows.Forms.TextBox txtN7;
+        private System.Windows.Forms.TextBox txtN8;
+        private System.Windows.Forms.TextBox txtN9;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtMediana;
+        private System.Windows.Forms.TextBox txtModa;
+        private System.Windows.Forms.TextBox txtDp;
+        private System.Windows.Forms.TextBox txtSimetria;
+        private System.Windows.Forms.TextBox txtPrimeiroQ;
+        private System.Windows.Forms.TextBox txtSegundoQ;
+        private System.Windows.Forms.TextBox txtTerceiroQ;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Probabilidade;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnLimpa;
+    }
+}
+
---
+++ ProjetoEstatistica v0.01/ProjetoEstatistica v0.01/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoEstatistica_v0._01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+            double[] amostra = new double[9];
+            amostra[0] = double.Parse(txtN1.Text);
+            amostra[1] = double.Parse(txtN2.Text);
+            amostra[2] = double.Parse(txtN3.Text);
+            amostra[3] = double.Parse(txtN4.Text);
+            amostra[4] = double.Parse(txtN5.Text);
+            amostra[5] = double.Parse(txtN6.Text);
+            amostra[6] = double.Parse(txtN7.Text);
+            amostra[7] = double.Parse(txtN8.Text);
+            amostra[8] = double.Parse(txtN9.Text);
+            //Calculo de média            
+            txtMedia.Text = Convert.ToString(CalculaMedia());
+            
+            //Desvio padrão                     
+            txtDp.Text = Convert.ToString(CalculaDesvio(amostra));
+
+            //Mediana
+            txtMediana.Text = Convert.ToString(CalculaMediana(amostra));
+
+            //Moda
+            txtModa.Text = Convert.ToString(CalculaModa(amostra));            
+
+            //Simetria            
+            txtSimetria.Text = Convert.ToString(CalculaSimetria(amostra));
+
+            // Primeiro Quartil
+            txtPrimeiroQ.Text = Convert.ToString(PrimeiroQuatil(amostra));
+
+            // Segundo Quatil/Mediana
+            txtSegundoQ.Text = Convert.ToString(CalculaMediana(amostra)) ;
+
+            // Terceiro Quatil
+            txtTerceiroQ.Text = Convert.ToString(TerceiroQuartil(amostra));    
+        }
+
+        private double CalculaMedia()
+        {
+            float n1, n2, n3, n4, n5, n6, n7, n8, n9;
+            n1 = float.Parse(txtN1.Text);
+            n2 = float.Parse(txtN2.Text);
+            n3 = float.Parse(txtN3.Text);
+            n4 = float.Parse(txtN4.Text);
+            n5 = float.Parse(txtN5.Text);
+            n6 = float.Parse(txtN6.Text);
+            n7 = float.Parse(txtN7.Text);
+            n8 = float.Parse(txtN8.Text);
+            n9 = float.Parse(txtN9.Text);
+
+            double media = (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9) / 9;            
+            return media;
+        }
+
+        private double CalculaMediana(double[] amostra)
+        {         
+            Array.Sort(amostra);
+            double mediana = amostra[4];//mediana = 9+1/2, posição 4 no vetor
+            return mediana;
+        }
+
+        private double CalculaDesvio(double[] amostra)
+        {            
+            double media = CalculaMedia();
+            double dp1 = 0;
+            double dp2 = 0;
+            double dpR = 0;
+            for(int i = 0; i <9; i++)
+            {
+                dp1 += Math.Pow((amostra[i] - media),2);//somatorio de cada elemento do array menos a media elevado ao quadrado
+                dp2 = dp1 / (9 - 1);
+                dpR = Math.Sqrt(dp2);
+                
+            }                               
+            
+            return dpR;
+            
+        }
+        private double CalculaModa(double[] amostra)
+        {           
+
+            int repeticao = 0;
+            double moda = 0;
+            int comparacao = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 9; j++)
+                {
+                    if (amostra[i] == amostra[j])
+                    {
+                        ++repeticao;
+                    }
+
+                }
+                if (repeticao > comparacao)
+                {
+                    moda = amostra[i];
+                    comparacao = repeticao;
+                }
+            }
+            return moda;
+        }
+        private double CalculaSimetria(double[] amostra)
+        {
+            double simetria = 0;          
+            simetria = 3 * (CalculaMedia() - CalculaMediana(amostra)) / CalculaDesvio(amostra);
+            return simetria;
+        }
+        private double PrimeiroQuatil(double[] amostra)
+        {         
+            Array.Sort(amostra);
+            double quartil1 = amostra[2];//Q1 = 9+1/4 = 2,5(Arredonda pra 3), posição 2 do vetor 
+            return quartil1;
+        }
+        private double TerceiroQuartil(double[] amostra)
+        {        
+            Array.Sort(amostra);
+            double quartil2 = amostra[6];
+            return quartil2;
+        }
+        private void BtnLimpa_Click(object sender, EventArgs e)
+        {
+            txtN1.Text = "";
+            txtN2.Text = "";
+            txtN3.Text = "";
+            txtN4.Text = "";
+            txtN5.Text = "";
+            txtN6.Text = "";
+            txtN7.Text = "";
+            txtN8.Text = "";
+            txtN9.Text = "";
+            txtMedia.Text = "";
+            txtMediana.Text = "";
+            txtModa.Text =  "";
+            txtDp.Text = "";
+            txtSimetria.Text = "";
+            txtPrimeiroQ.Text = "";
+            txtSegundoQ.Text = "";
+            txtTerceiroQ.Text = "";
+
+        }
+    }
+}
